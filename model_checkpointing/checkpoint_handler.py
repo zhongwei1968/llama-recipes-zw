55f3d1f0b634e6b19f8fb322ec8b915b260465fe
Li_Zhongwei <Li_zhongwei@imda.gov.sg>
2023-08-02 11:03:31 +0800

    use save_pretrained to save model

--- model_checkpointing/checkpoint_handler.py
+++ model_checkpointing/checkpoint_handler.py
@@ -153,12 +153,13 @@ def save_model_checkpoint(
         save_dir = Path.cwd() / folder_name
         save_dir.mkdir(parents=True, exist_ok=True)
         model_base_name = os.path.basename(cfg.model_name)
-        save_name = model_base_name + "-" + str(epoch) + ".pt"
+        save_name = model_base_name + "-" + str(epoch)
         save_full_path = str(save_dir) + "/" + save_name
 
         # save model
-        torch.save(cpu_state, save_full_path)
+        # torch.save(cpu_state, save_full_path)
 
+        model.save_pretrained(save_full_path)
         
         print(f"model checkpoint saved for epoch {epoch} at {save_full_path}\n")
       
